f92bcb4990e4c75b26e029bb3e00518697a86ff6
vi-vi-3482 <haider_al@me.com>
2026-04-05 13:56:01 -0700

    added finally block to release cams

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
     interface = llm.LLMinterface("models/Qwen3.5-4B-Q4_K_M.gguf", tools.tool_json_list)
     cam = webcam.Webcam(0, (1920, 1080))
     depth_cam = depthcam.RealSense()
-
-    while True:
-        interface.get_text()
-        interface.send_message_with_tools(cam, depth_cam)
-        interface.print_message()
+    try:
+        while True:
+            interface.get_text()
+            interface.send_message_with_tools(cam, depth_cam)
+            interface.print_message()
+    except Exception as e:
+        print(e)
+    finally:
+        cam.stop_webcam()
+        depth_cam.stop()
 
 
 if __name__ == "__main__":
